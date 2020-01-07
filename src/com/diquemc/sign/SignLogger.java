a30c86f3c09a2149cff3ad093ed7eb0b2072c980
Hector Romero <hector22x@gmail.com>
2020-01-07 14:28:09 -0300

    Removed unnecesary instance variable

--- src/com/diquemc/sign/SignLogger.java
+++ src/com/diquemc/sign/SignLogger.java
@@ -4,10 +4,9 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SignLogger extends JavaPlugin {
-    private final SLBlockListener blockListener = new SLBlockListener();
 
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this.blockListener, this);
+        getServer().getPluginManager().registerEvents(new SLBlockListener(), this);
         loadConfiguration();
         getLogger().info(" version [" + getDescription().getVersion() + "] loaded");
     }
